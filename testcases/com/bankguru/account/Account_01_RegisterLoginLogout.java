935eb9ba75b8d899ef0949e68b78c31666a9f66e
tad19 <tad19@truonganhdung>
2018-11-11 16:41:01 +0700

    Commit

--- testcases/com/bankguru/account/Account_01_RegisterLoginLogout.java
+++ testcases/com/bankguru/account/Account_01_RegisterLoginLogout.java
@@ -1,5 +1,6 @@
 package com.bankguru.account;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
@@ -7,50 +8,74 @@ import org.testng.annotations.AfterClass;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Account_01_RegisterLoginLogout {
 	WebDriver driver;
-	
+	String email, userID, password, loginURL;
+
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 		driver.get("http://demo.guru99.com/v4/");
-		
-		email = "automationtesting" + randomNumber() + "@gmail.com";
+
+		email = "automation" + randomNumber() + "@gmail.com";
 	}
 
 	@Test
-	public void TC_01_RegisterToSystem() {
-		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr155533");
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("aqAtAda");
+	public void TC_01_RegisterToSystem() throws Exception {
+		loginURL = driver.getCurrentUrl();
+
+		// register
+		driver.findElement(By.xpath("//a[text()='here']")).click();
+		driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+		Thread.sleep(1000);
+		// get info after registering
+		userID = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
+		password = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
+		// System.out.println(userID);
+		// System.out.println(password);
+	}
+
+	@Test
+	public void TC_02_LoginToSystem() throws Exception {
+		driver.get(loginURL);
+
+		// login
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(userID);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
 
-		
-	}
-
-	@Test
-	public void TC_02_LoginToSystem() {
+		// verify loggin successfully
+		Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//td[contains(text(),'Manger Id : " + userID + "')]")).isDisplayed());
+		Thread.sleep(1000);
 	}
 
 	@Test
 	public void TC_03_LogoutToSystem() {
+		driver.findElement(By.xpath("//a[text()='Log out']")).click();
+
+		Alert alertLogout = driver.switchTo().alert();
+		alertLogout.accept();
 	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
-	
+
 	public int randomNumber() {
 		Random rand = new Random();
 		int random = rand.nextInt(99999) + 1;
 		return random;
 	}
-
 }
